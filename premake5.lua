c3a8a0949951f6d6bb281f43a87909fd9db9e5c7
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2020-09-24 20:51:53 +0300

    Update premake and gitignore files

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,33 @@
 project "ImGui"
-	kind "StaticLib"
-	language "C"
-  
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    kind "StaticLib"
+    language "C++"
 
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	filter "system:windows"
-		systemversion "latest"
-    cppdialect "C++17"
-    staticruntime "on"
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "on"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
